b68a2efa010c5e29a3247675d6c4d2aa361c9485
Saki <sbasken@gmail.com>
2023-02-21 17:52:37 -0800

    add filter function to shoppinglist.js

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  let [filterBy, setFilterBy] = useState("All")
+
+  function handleChange(e) {
+    setFilterBy(e.target.value)
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return item.category === filterBy
+    }
+  })
+
+  const selectedCategory = itemsToDisplay.map((item)=> (
+    <Item key={item.id} name={item.name} category={item.category} />
+  ))
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +32,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+       {selectedCategory}
       </ul>
     </div>
   );
